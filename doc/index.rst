b6dcf977bf5fb89242e3b4889a4f07dd47a70e87
Brian Beach <coder@beachfamily.net>
2013-12-20 09:06:55 -0800

    Turn bstat into a package.

---
+++ doc/index.rst
@@ -0,0 +1,24 @@
+.. TinyMock documentation master file, created by
+   sphinx-quickstart on Fri Feb 11 14:20:15 2011.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+bstat - handy data processing and statistics for Python
+===============================================
+
+.. automodule:: tinymock
+
+TestCase Class
+--------------
+
+.. autoclass:: bstat.Table
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(name='bstat',
+      version=version,
+      description="Data handlig for statistics, with connections to Google Charts",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Brian Beach',
+      author_email='bwbeach@beachfamily.net',
+      url='http://www.beachfamily.net/bstat/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
